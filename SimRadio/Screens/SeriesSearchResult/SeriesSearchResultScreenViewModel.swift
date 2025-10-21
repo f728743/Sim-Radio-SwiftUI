3953d242481688f7445a7a657918955058365de4
Alexey Vorobyov <vorobyov.alexey@gmail.com>
2025-10-21 07:53:50 +0300

    Show series search result

--- SimRadio/Screens/SeriesSearchResult/SeriesSearchResultScreenViewModel.swift
+++ SimRadio/Screens/SeriesSearchResult/SeriesSearchResultScreenViewModel.swift
@@ -12,6 +12,7 @@ import SwiftUI
 class SeriesSearchResultScreenViewModel {
     struct Display {
         let cover: Cover
+        let series: Series
     }
 
     let seriesDTO: APISimRadioSeriesDTO
@@ -21,14 +22,14 @@ class SeriesSearchResultScreenViewModel {
         seriesDTO = series
     }
 
-    var display: Display? {
-        guard let series else { return nil }
+    var display: Display {
+        guard let series else { return .placeholder }
         let result = Display(
             origin: seriesDTO.url,
             dto: series,
             stations: seriesDTO.stations.map(\.id)
         )
-        return result
+        return result ?? .placeholder
     }
 
     func load() async throws {
@@ -37,24 +38,48 @@ class SeriesSearchResultScreenViewModel {
         let radio = try JSONDecoder().decode(SimRadioDTO.GameSeries.self, from: jsonData.0)
         series = radio
     }
+
+    func play() {
+        print("play ", display.series.title)
+    }
+
+    func addSeries() {
+        print("add ", display.series.title)
+    }
 }
 
 private extension SeriesSearchResultScreenViewModel {}
 
 extension SeriesSearchResultScreenViewModel.Display {
     struct Cover {
-        let title: String
         let image: URL?
+        let title: String
+    }
+
+    struct Series {
+        let artwork: URL?
+        let title: String
+        let stationCount: Int
+        var description: LocalizedStringKey {
+            "^[\(stationCount) station](inflect: true)"
+        }
+    }
+
+    static var placeholder: Self {
+        .init(
+            cover: .init(image: nil, title: ""),
+            series: .init(artwork: nil, title: "", stationCount: 0)
+        )
     }
 }
 
 extension APISimRadioSeriesDTO {
     static var stub: APISimRadioSeriesDTO {
         .init(
-            id: "String",
+            id: "",
             url: "",
-            title: "String",
-            logo: "String",
+            title: "",
+            logo: "",
             stations: []
         )
     }
@@ -63,12 +88,23 @@ extension APISimRadioSeriesDTO {
 extension SeriesSearchResultScreenViewModel.Display {
     init?(origin: String, dto: SimRadioDTO.GameSeries, stations _: [String]) {
         guard let origin = URL(string: origin) else { return nil }
+        let coverURL = origin
+            .deletingLastPathComponent()
+            .appendingPathComponent(dto.meta.cover.image)
+
+        let seriesAwtworkURL = origin
+            .deletingLastPathComponent()
+            .appendingPathComponent(dto.meta.logo)
+
         self.init(
             cover: .init(
-                title: dto.meta.cover.title,
-                image: origin
-                    .deletingLastPathComponent()
-                    .appendingPathComponent(dto.meta.cover.image)
+                image: coverURL,
+                title: dto.meta.cover.title
+            ),
+            series: Series(
+                artwork: seriesAwtworkURL,
+                title: dto.meta.title,
+                stationCount: dto.stations.count
             )
         )
     }
